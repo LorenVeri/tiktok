3c6be988f48bbd66312b185c86d7246b9815a1d0
Vũ Ngọc Long <longvn@ftech.ai>
2023-02-16 11:46:36 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "~/components/Button";
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <Button></Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tiktok - UI
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) Layout = route.layout;
+                        else if (route.layout === null) Layout = Fragment;
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                            <Link to="/following">Following</Link>
+                            <Link to="/upload">Upload</Link>
+                            <Link to="/search">search</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
